79ed43a102c5e86910eb3ca585179839f150c94a
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-02-19 11:27:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const server = express();
 const hostname = "127.0.0.1";
 const port = 8080;
 
+server.get("/heartbeat", (req, res) => {
+  res.json({
+    is: "json",
+  });
+});
+
 server.listen(port, hostname, function () {
   console.log(`server is listening at port ${port} `);
 });
