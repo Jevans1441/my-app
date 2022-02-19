c1a299f540c9a6fedc8852f65e658ba82bcd9084
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-02-19 12:36:54 -0500

    render ui with react build folder

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const { application } = require("express");
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const server = express();
 const hostname = "127.0.0.1";
-const port = 8080;
+const { PORT } = process.env;
+
+server.use(express.static(path.resolve(__dirname + "/react-ui/build")));
 
 server.get("/heartbeat", (req, res) => {
   res.json({
-    is: "json",
+    is: "working",
   });
 });
 
-server.listen(port, hostname, function () {
-  console.log(`server is listening at port ${port} `);
+server.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./react-ui/build", "index.html"));
+});
+
+server.listen(PORT, hostname, () => {
+  console.log(`server is listening at ${hostname}:${PORT}`);
 });
